37a1c1c16d3267bc93b99f688983fb427e2baf38
Taylor <taylormyrick@gmail.com>
2023-02-07 23:40:47 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        return this.title + this. author + this.pages + this.read;
+    }
+}
