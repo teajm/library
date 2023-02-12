0ec24be29784514570424912baeefe8e98fb1388
Taylor <taylormyrick@gmail.com>
2023-02-11 22:45:53 -0600

    adjusted add divs to only show latest book

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let library = [];
 let cbTrue = "yes!";
 let cbFalse = "heck no";
+let end = 0;
+
 const container = document.querySelector('.container');
 const addButton = document.querySelector('.addButton');
 const newBookButton = document.querySelector('.newBookButton');
-const btnAddBook = document.querySelector('.btnAddBook');
-
+const addBookButton = document.querySelector('.addBookButton');
+const bookContainerDiv = document.querySelector('.cards-block');
 // addButton.addEventListener ('click', renderLibrary());
 newBookButton.addEventListener ('click', openForm());
 
@@ -20,17 +22,15 @@ function openForm(){
     // clearAllRequiredFields();
 }
 
-addButton.addEventListener("click", function () {
-    for(let i = 0; i < library.length; i++){
-        createDisplay(library[i]);
-    }
-})
+
+
 newBookButton.addEventListener ('click', function() {
     document.getElementById('formContainer').style.display='block'
     document.getElementById('formPopup').reset();
     // clearAllRequiredFields();
 })
-btnAddBook.addEventListener("click",function(){
+
+addBookButton.addEventListener("click",function(){
     let title = document.getElementById("titleInput").value;
     let author = document.getElementById("authorInput").value;
     let pages = document.getElementById("pagesInput").value;
@@ -41,33 +41,55 @@ btnAddBook.addEventListener("click",function(){
     else{
         addBookToLibrary(title,author,pages,cbFalse);
     }
-
 })
-
 
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-
 }
 
 function addBookToLibrary(title, author, pages, read){
     const bookObj = new Book(title,author,pages,read);
     library.push(bookObj);
+    renderLibrary();
 }
-function renderLibrary(){
 
+function renderLibrary(){
+    if(end ===0){
+        for(let i = 0; i < library.length; i++){
+                createDisplay(library[i]);
+        }
+        end = library.length;
+    }
+    else{
+        clearDivs();
+        for(let i = 0; i < library.length; i++){
+            createDisplay(library[i]);
+    }
+    }
 }
+
+function clearDivs(){
+    while(bookContainerDiv.firstChild) {
+        bookContainerDiv.removeChild(bookContainerDiv.firstChild);
+      }
+}
+function deleteDiv(div){
+    bookContainerDiv.removeChild(bookContainerDiv.firstChild);
+}
+
 function createDisplay(obj){
-    const bookContainerDiv = document.createElement('div');
     const bookDiv = document.createElement('div');
     const titleDiv = document.createElement('div');
     const authorDiv = document.createElement('div');
     const pagesDiv = document.createElement('div');
     const readDiv = document.createElement('div');
-
+    titleDiv.setAttribute("id","booktitle");
+    authorDiv.setAttribute("id","bookauthor");
+    pagesDiv.setAttribute("id","bookpages");
+    readDiv.setAttribute("id","bookstatus");
     bookContainerDiv.setAttribute('data', library.indexOf(obj));
     container.appendChild(bookContainerDiv);
 
@@ -91,14 +113,6 @@ function createDisplay(obj){
     bookDiv.appendChild(readDiv);
 }
 addBookToLibrary("loco","moko","poko","boko");
-addBookToLibrary("loco","moko","poko","boko");
-addBookToLibrary("loco","moko","poko","boko");
-addBookToLibrary("loco","moko","poko","boko");
-addBookToLibrary("loco","moko","poko","boko");
-addBookToLibrary("loco","moko","poko","boko");
-addBookToLibrary("loco","moko","poko","boko");
-addBookToLibrary("loco","moko","poko","boko");
-addBookToLibrary("loco","moko","poko","boko");
 
 
-
+// renderLibrary(0);
